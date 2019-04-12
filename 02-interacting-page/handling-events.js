61c444a59822e7d6a030b289e3dadf9b4e5ff97f
nitayneeman <nitayneeman@users.noreply.github.com>
2019-04-12 15:00:13 +0300

    Added example

---
+++ 02-interacting-page/handling-events.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Called when the whole page has loaded
+  page.once('load', () => console.info('Page loaded!'));
+
+  // Called after the page is closed
+  page.once('close', () => console.info('Page closed!'));
+
+  await page.goto('https://pptr.dev');
+
+  await browser.close();
+})();
