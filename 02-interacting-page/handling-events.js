dfba55213e31319fe4d8f23e6307adffa932aed9
nitayneeman <nitayneeman@users.noreply.github.com>
2019-04-12 19:07:41 +0300

    Improved example

--- 02-interacting-page/handling-events.js
+++ 02-interacting-page/handling-events.js
@@ -5,15 +5,21 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   // Emitted when the DOM is parsed and ready (without waiting for resources)
-  page.once('domcontentloaded', () => console.info('Page is loaded!'));
+  page.once('domcontentloaded', () => console.info('✅ DOM is ready'));
 
   // Emitted when the page is fully loaded
-  page.once('load', () => console.info('Page is loaded!'));
+  page.once('load', () => console.info('✅ Page is loaded'));
+
+  // Emitted when a script within the page uses `console`
+  page.on('console', message => console[message.type()](`👉 ${message.text()}`));
 
   // Emitted after the page is closed
-  page.once('close', () => console.info('Page is closed!'));
+  page.once('close', () => console.info('✅ Page is closed'));
 
   await page.goto('https://pptr.dev');
+
+  // Triggers `console` event
+  await page.evaluate(() => console.info('Console message within the page'));
 
   // Triggers `close` event
   await page.close();
