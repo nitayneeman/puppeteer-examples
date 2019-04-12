b36cd947797919e123788f7c1cf3937090a67781
nitayneeman <nitayneeman@users.noreply.github.com>
2019-04-12 19:12:37 +0300

    Improved example

--- 02-interacting-page/handling-events.js
+++ 02-interacting-page/handling-events.js
@@ -13,13 +13,22 @@ const puppeteer = require('puppeteer');
   // Emitted when a script within the page uses `console`
   page.on('console', message => console[message.type()](`👉 ${message.text()}`));
 
+  // Emitted when a script within the page uses `alert`, `prompt`, `confirm` or `beforeunload`
+  page.on('dialog', async dialog => {
+    console.info(`👉 ${dialog.message()}`);
+    await dialog.dismiss();
+  });
+
   // Emitted after the page is closed
   page.once('close', () => console.info('✅ Page is closed'));
 
   await page.goto('https://pptr.dev');
 
   // Triggers `console` event
-  await page.evaluate(() => console.info('Console message within the page'));
+  await page.evaluate(() => console.info('A console message within the page'));
+
+  // Triggers `dialog` event
+  await page.evaluate(() => alert('An alert within the page'));
 
   // Triggers `close` event
   await page.close();
