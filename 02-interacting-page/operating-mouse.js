f2ce3ab628e00b73d082d95d4b13985f481e9778
nitayneeman <nitayneeman@users.noreply.github.com>
2019-05-01 21:54:19 +0300

    Improved example

--- 02-interacting-page/operating-mouse.js
+++ 02-interacting-page/operating-mouse.js
@@ -8,8 +8,19 @@ const puppeteer = require('puppeteer');
   await page.goto('https://pptr.dev');
   await page.waitForSelector('sidebar-component');
 
+  // Hovers the second link inside the API sidebar
+  await page.mouse.move(40, 150);
+
   // Clicks the second link and triggers `mouseup` event after 1000ms
   await page.mouse.click(40, 150, { delay: 1000 });
+
+  // Drags the mouse from a point
+  await page.mouse.move(0, 0);
+  await page.mouse.down();
+
+  // Drops the mouse to another point
+  await page.mouse.move(100, 100);
+  await page.mouse.up();
 
   await browser.close();
 })();
