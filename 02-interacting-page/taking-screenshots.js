88931de4113fbfedf3b512bb3afa887ee62d1a7b
nitayneeman <nitayneeman@users.noreply.github.com>
2019-06-06 21:56:15 +0300

    Improved example

--- 02-interacting-page/taking-screenshots.js
+++ 02-interacting-page/taking-screenshots.js
@@ -4,10 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Navigates to URL
   await page.goto('https://pptr.dev');
-
-  // Waits until the `title` meta element is rendered
   await page.waitForSelector('title');
 
   // Takes a screenshot of the whole page
