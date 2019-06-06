2daf365d4ab83d6e7d59a23d03a685ec4504ebe7
nitayneeman <nitayneeman@users.noreply.github.com>
2019-06-06 22:03:04 +0300

    Improved example

--- 02-interacting-page/taking-screenshots.js
+++ 02-interacting-page/taking-screenshots.js
@@ -4,11 +4,12 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  await page.setViewport({ width: 1920, height: 1080 });
   await page.goto('https://pptr.dev');
   await page.waitForSelector('title');
 
   // Takes a screenshot of the whole page
-  await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  await page.screenshot({ path: 'screenshot.png' });
 
   await browser.close();
 })();
