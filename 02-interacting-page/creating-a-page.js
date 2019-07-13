b07fbe55f4486ad98ea779b1faf1606da55d545a
nitayneeman <nitayneeman@users.noreply.github.com>
2019-07-13 12:32:10 +0300

    Improved example

--- 02-interacting-page/creating-a-page.js
+++ 02-interacting-page/creating-a-page.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch();
+
+  // Creates a new page on the default browser context
   const page = await browser.newPage();
   console.info(page);
+
   await browser.close();
 })();
