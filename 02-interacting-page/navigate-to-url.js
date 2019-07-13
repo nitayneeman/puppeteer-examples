d4ec2f47477281016f53d8408d8e4825e83ce2ac
nitayneeman <nitayneeman@users.noreply.github.com>
2019-07-13 12:11:32 +0300

    Improved example

--- 02-interacting-page/navigate-to-url.js
+++ 02-interacting-page/navigate-to-url.js
@@ -1,11 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   // Instructs the blank page to navigate a URL
   await page.goto('https://pptr.dev');
+
+  // Waits until the `title` meta element is rendered
+  await page.waitForSelector('title');
 
   // Fetches page's title
   const title = await page.title();
