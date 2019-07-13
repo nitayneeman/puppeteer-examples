827f423335744f0f888666cc3628ef31b46969d9
nitayneeman <nitayneeman@users.noreply.github.com>
2019-07-13 16:47:40 +0300

    Improved example

--- 02-interacting-page/operating-keyboard.js
+++ 02-interacting-page/operating-keyboard.js
@@ -6,6 +6,8 @@ const puppeteer = require('puppeteer');
 
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto('https://pptr.dev');
+
+  // Waits until the toolbar is rendered
   await page.waitForSelector('toolbar-component');
 
   // Focuses the search input
@@ -14,7 +16,7 @@ const puppeteer = require('puppeteer');
   // Types the text into the focused element
   await page.keyboard.type('Keyboard', { delay: 100 });
 
-  // Choosing the second result
+  // Choosing the third result
   await page.keyboard.press('ArrowDown', { delay: 200 });
   await page.keyboard.press('ArrowDown', { delay: 200 });
   await page.keyboard.press('Enter');
