7588bff5bf74bf8852f574043055c4d8a4cc93a9
nitayneeman <nitayneeman@users.noreply.github.com>
2020-07-05 20:47:37 +0300

    fix: import devices correctly

--- 02-interacting-page/emulating-devices.js
+++ 02-interacting-page/emulating-devices.js
@@ -1,12 +1,12 @@
-const puppeteer = require('puppeteer');
-const devices = require('puppeteer/DeviceDescriptors');
+const puppeteer = require("puppeteer");
+const { devices } = puppeteer;
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.emulate(devices['iPhone X']);
-  await page.goto('https://pptr.dev');
+  await page.emulate(devices["iPhone X"]);
+  await page.goto("https://pptr.dev");
 
   await browser.close();
 })();
